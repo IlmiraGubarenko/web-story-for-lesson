f4bf240cd447db75f676d6a7d5bf324bd315d2f3
IlmiraGubarenko <ilmira.gubarenko@gmail.com>
2017-09-04 22:45:39 +0300

    добавлена страница product.html, сделан блок product-carousel

--- src/js/init.js
+++ src/js/init.js
@@ -66,4 +66,13 @@ $( function() {
 
     })
 
+    // flexslider init
+    $(window).load(function() {
+        $('.flexslider').flexslider({
+            animation: "slide",
+            controlNav: "thumbnails"
+        });
+    });
+
+
 } );
